3623652b8c5516859582ee55b82b8becb27cf075
Camilo Gaitan <camilo.gaitan09@gmail.com>
2023-04-07 15:35:59 -0500

    PWA

--- next.config.js
+++ next.config.js
@@ -1,23 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['placeimg.com', 'api.lorem.space', 'product.hstatic.net', 'm.media-amazon.com', 'www.vienquangmobile.com', 'store.storeimages.cdn-apple.com', 'synnexfpt.com', 'footgearh.vn', 'www.readersdigest.ca', 'firebasestorage.googleapis.com', ''],
   },
-  reactStrictMode: true,
-  // env: {
-  //   customKey: 'customValue',
-  // },
-  // basePath: "/dist",
-  // compress: true,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/hola',
-  //       destination: '/hello',
-  //       permanent: true,
-  //     },
-  //   ]
-  // }
 };
 
-module.exports = nextConfig;
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+});
+
+module.exports = withPWA(nextConfig);
