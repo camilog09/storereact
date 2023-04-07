97f39c9aede252ceee4eec3297d2f15e76b63d33
Camilo Gaitan <camilo.gaitan09@gmail.com>
2023-04-07 13:33:47 -0500

    Iniciando a desplegar

---
+++ src/templates/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <span>Error 404 | No encontrado</span>
+            <h4>Oops! Buscamos por todo lado y no enctramos lo que buscadas</h4>
+            <p>Sigue intentando con las opciones de menú</p>
+        </div>
+    );
+};
+
+export default NotFound;
